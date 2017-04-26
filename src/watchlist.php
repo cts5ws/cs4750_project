a8962c691e9845d8340812274233c09ee2163ef7
Chase Deets <chd5hq@virginia.edu>
2017-04-26 09:01:13 -0400

    SpElLInG iz GuD

--- src/watchlist.php
+++ src/watchlist.php
@@ -157,7 +157,7 @@
     	<div class="row">
     		<div class="col-lg-12">
     			 <h1><i class="fa fa-fw fa-list"></i>Watchlist Manager</h1>
-    			 <h4>Build the best roster, beat the best roster.</h4>
+    			 <h4>Build your best roster, beat the best roster.</h4>
     		</div>
     	</div>
     <div class="row">
