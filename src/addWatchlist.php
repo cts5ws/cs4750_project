2398daad300f0a18c02253d833bb1ebdc3cfc69a
Chase Deets <chd5hq@virginia.edu>
2017-04-26 05:01:31 -0400

    Watchlist creation working

--- src/addWatchlist.php
+++ src/addWatchlist.php
@@ -1,7 +1,45 @@
 <?php
-
+	
 
 
 ?>
 
-<h3> Add Watchlist Page </h3>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>My Watchlist</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/acc.css" rel="stylesheet">
+
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+<div class='container'>
+	<div class='row'>
+		<div class='col-lg-12'>
+			<form name="createList" action="createList.php" method="post">
+				<div class="form-group">
+					<label for="listname">Watchlist Name:</label>
+					<input type="text" class="form-control" name="listname">
+				</div>
+				<input type="submit" class="btn btn-info" value="Create Watchlist!">
+			</form>
+		</div>
+	</div>
+</div>
+
+
+</body>
+</html>
---
+++ src/createList.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+include_once("./library.php"); // To connect to the database
+$con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+ // Check connection
+if (mysqli_connect_errno()){
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$listname = $_POST["listname"];
+$fan_id = $_SESSION["fan_id"];
+//insert into watchlist
+mysqli_query($con, "insert into `Watchlist` (Watchlist_Name) values ('$listname')");
+
+//get watchlist id, fan_id and insert into creates
+$result = mysqli_query($con, "Select Watchlist_ID from Watchlist where Watchlist_Name = '$listname'");
+$result = mysqli_fetch_assoc($result);
+$newID = $result["Watchlist_ID"];
+
+mysqli_query($con, "insert into Creates (Fan_ID, Watchlist_ID) values ('$fan_id', '$newID')");
+
+
+header("Refresh:0; url=watchlist.php");
+
+?>
